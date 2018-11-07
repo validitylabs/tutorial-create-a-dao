7d2481570e06920b9f753b23dde326cd77a6bd09
ChenchenYo <yuqianchen93@gmail.com>
2018-11-07 11:12:00 +0100

    latest

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -9,6 +9,7 @@ class LandingPage extends React.Component {
         <section>Dear #steps18 participants, welcome to the DAO workshop!
         This website helps you to keep up with the workshop.
         Please click on titles to go up/down levels.
+        You can also join the <a href="https://t.me/joinchat/IL8GDFYyCY0bTlWpgbP8Tg">telegram group</a>
         </section>
         
         <ul>
